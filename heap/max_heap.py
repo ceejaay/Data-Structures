2f24d7b54bad82aa0798d407cd440bd9a76f334a
Chad Jemmett <chad.jemmett@gmail.com>
2019-03-06 16:27:18 -0700

    Working on sift down. Recursion

--- heap/max_heap.py
+++ heap/max_heap.py
@@ -1,7 +1,7 @@
 class Heap:
   def __init__(self):
-    self.storage = []
-    # self.storage = [98, 67, 81, 43, 51, 78, 54, 24, 27, 30, 26]
+    # self.storage = []
+    self.storage = [999, 67, 81, 43, 51, 78, 54, 24, 27, 30, 1]
 
   def insert(self, value):
       self.storage.append(value)
@@ -10,8 +10,19 @@ class Heap:
 
 
   def delete(self):
-      self.storage.pop(0)
-      return self._bubble_up(len(self.storage) -1)
+      last_index =  self.storage[len(self.storage) - 1]
+      # last index will be 1
+      # this swaps 1 and 999
+      self.storage[0], self.storage[len(self.storage) - 1] = self.storage[len(self.storage) - 1],  self.storage[0]
+      # this pops 999
+
+      self.storage.pop(-1)
+      # call sift down
+      self._sift_down(0)
+      print(self.storage)
+
+
+
 
   def get_max(self):
       return self.storage[0]
@@ -28,21 +39,38 @@ class Heap:
           self.storage[parent], self.storage[new_value] = self.storage[new_value], self.storage[parent]
           new_value = parent
           parent = (new_value - 1) // 2
-          # print(f'new_value: {self.storage[new_value]} new value index: {new_value}, parent index: {parent}, parent value: {self.storage[parent]}', )
+          # print(f'new_value: {self.storage[new_value]} new value index: {new_value}, parent index: {parent}, parent value: {self.storage[parent]}', )kk
 
       # print('top value: ', self.storage[0])
       # print('array', self.storage)
 
-
-
-
   def _sift_down(self, index):
-      pass
+      # left 2i + 1
+      # right 2i + 2
+      # the value at 0 is 1
+      # parent == 0
+      parent = index
+      # get last index which is 9
+      last_index = len(self.storage) - 1
+      # while 1 < 30
+      while self.storage[parent]< self.storage[last_index]:
 
+          left_child = 2 * parent + 1
+          right_child = 2 * parent + 2
+          if self.storage[left_child] > self.storage[right_child]:
+             self.storage[left_child], self.storage[parent] = self.storage[parent], self.storage[left_child]
+             parent = left_child
+             print(f"p: {self.storage[index]}, L: {self.storage[left_child]} R: {self.storage[right_child]}")
+             print(self.storage)
+          else:
+              self.storage[right_child], self.storage[parent] = self.storage[parent], self.storage[right_child]
+              parent = right_child 
+              print(f"p: {self.storage[index]}, L: {self.storage[left_child]} R: {self.storage[right_child]}")
+              print(self.storage)
 
-
-# h = Heap()
+h = Heap()
 # print(h.insert(999))
 # print(h.storage)
+h.delete()
 
 
